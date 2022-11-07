297d062c31ac56873b8b3512c8133085957abbce
dubh-SKM <paradoxfromthebay@gmail.com>
2022-11-06 18:04:31 -0800

    completed assignment

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+// This is a single line comment
+
+
+/* This is a multi-line comment
+   which takes place across multiple lines */
+
+
+let name = 'Elijah';
+console.log(name);
+
+let favoriteFood = 'Trader Joes Spinach and Kale Greek Yogurt Dip';
+console.log(favoriteFood);
